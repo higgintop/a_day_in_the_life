d84967bb19e7588e0fdd56d6ee902cb5d2df1b84
Allison Higginbotham <higgintop@gmail.com>
2015-06-02 21:23:21 -0500

    Auto login user when signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path
+      auto_login(@user)
+      redirect_to root_path, notice: "Welcome, #{@user.name}"
     else
       flash.alert = "Please fix the errors below to continue."
       render :new

605b4f4f1ffeb9b8d315399f154bcf9a27f828e5
Allison Higginbotham <higgintop@gmail.com>
2015-06-17 09:53:28 -0500

    Updated tests for when user signs up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       auto_login(@user)
-      redirect_to root_path, notice: "Welcome, #{@user.name}"
+      redirect_to root_path, notice: "Welcome, #{@user.name}!"
     else
       flash.alert = "Please fix the errors below to continue."
       render :new
--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -17,5 +17,21 @@ feature "User Signs Up" do
     click_button "Go!"
     page.should have_css(".journals_btn")
   end
+
+   scenario "Sad Path" do
+    fill_in "Name", with: ""
+    fill_in "Email", with: "joe@example.com"
+    fill_in "Password", with: "password1"
+    fill_in "Password confirmation", with: "password1"
+    click_button "Go!"
+    page.should have_content("can't be blank")
+    fill_in "Name", with: "Joe"
+    fill_in "Email", with: "joe@example.com"
+    fill_in "Password", with: "password1"
+    fill_in "Password confirmation", with: "password1"
+    click_button "Go!"
+    page.should have_content("Welcome, Joe!")
+
+   end
 end
 

04002b0fb0aee3cfb2f31f831002d27873ab84d3
Allison Higginbotham <higgintop@gmail.com>
2015-06-17 10:23:46 -0500

    Updated test for viewing list of journals

--- spec/fabricators/journal_fabricator.rb
+++ spec/fabricators/journal_fabricator.rb
@@ -1,3 +1,4 @@
 Fabricator(:journal) do
+  user(fabricator: :user)
   title     { Faker::Name }
 end
--- spec/features/user_manage_journals_spec.rb
+++ spec/features/user_manage_journals_spec.rb
@@ -6,6 +6,9 @@ feature "Users manage journals" do
 
   before do
     user = Fabricate(:user, name: "Jenny")
+    Fabricate(:journal, title: "Sleep Tracking", user: user)
+    Fabricate(:journal, title: "Movie Reviews", user: user)
+    Fabricate(:journal, title: "Travel", user: user)
     visit root_path
     click_on "Sign In"
     fill_in "Email", with: user.email
@@ -13,40 +16,29 @@ feature "Users manage journals" do
     click_button "Go!"
   end
 
-  scenario "viewing journal list when empty" do
-    visit root_path
-    click_on "My Journals"
-    page.should have_content("There aren't any journals yet")
-    page.should_not have_content("Sort")
-  end
-
   scenario "viewing list of journals" do
-    Fabricate(:journal, title: "Sleep Tracking")
-    Fabricate(:journal, title: "Movie Reviews")
-    Fabricate(:journal, title: "Travel")
     visit root_path
-    click_on "My Journals"
-    current_path.should == "/journals" # We wouldn't normally test the exact path
-    within("ul#journals") do
-      page.should have_css("li:nth-child(1)", text: "Movie Reviews")
-      page.should have_css("li:nth-child(2)", text: "Sleep Tracking")
-      page.should have_css("li:nth-child(3)", text: "Travel")
+    click_on "Journals"
+    within("div.journals-container") do
+      page.should have_css(".journal:nth-child(5) ul li:nth-child(2)", text: "Movie Reviews")
+      page.should have_css(".journal:nth-child(6) ul li:nth-child(2)", text: "Sleep Tracking")
+      page.should have_css(".journal:nth-child(7) ul li:nth-child(2)", text: "Travel")
     end
     page.should_not have_css("a", text: "Sort: Alphabetical")
     page.should have_content("Sort: Alphabetical")
     click_on "Sort: Date"
-    within("ul#journals") do
-      page.should have_css("li:nth-child(1)", text: "Sleep Tracking")
-      page.should have_css("li:nth-child(2)", text: "Movie Reviews")
-      page.should have_css("li:nth-child(3)", text: "Travel")
+    within("div.journals-container") do
+      page.should have_css(".journal:nth-child(5) ul li:nth-child(2)", text: "Sleep Tracking")
+      page.should have_css(".journal:nth-child(6) ul li:nth-child(2)", text: "Movie Reviews")
+      page.should have_css(".journal:nth-child(7) ul li:nth-child(2)", text: "Travel")
     end
     page.should_not have_css("a", text: "Sort: Date")
     page.should have_content("Sort: Date")
     click_on "Sort: Alphabetical"
-    within("ul#journals") do
-      page.should have_css("li:nth-child(1)", text: "Movie Reviews")
-      page.should have_css("li:nth-child(2)", text: "Sleep Tracking")
-      page.should have_css("li:nth-child(3)", text: "Travel")
+    within("div.journals-container") do
+      page.should have_css(".journal:nth-child(5) ul li:nth-child(2)", text: "Movie Reviews")
+      page.should have_css(".journal:nth-child(6) ul li:nth-child(2)", text: "Sleep Tracking")
+      page.should have_css(".journal:nth-child(7) ul li:nth-child(2)", text: "Travel")
     end
   end
 end
